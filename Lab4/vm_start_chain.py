27abb4f37f51d80e6bfa164b2bc92ea7cbe2f82f
Jiwen37 <jiwenli@usc.edu>
2025-09-27 18:39:56 -0700

    Update vm_start_chain.py

--- Lab4/vm_start_chain.py
+++ Lab4/vm_start_chain.py
@@ -10,21 +10,23 @@ def on_connect(client,userdata,flags,rc):
 
     client.message_callback_add("cynthliu/pong", on_message_from_pong)
 
+    client.publish("cynthliu/ping", str(num))
+    print("Publishing initial number to ping: " + str(START_NUMBER))
+
 def on_message_from_pong(client, userdata, message):
-    new_num = int(message.payload.decode())
-    print(message.payload.decode())
-    if(num!=new_num):
-        num = new_num+1
+    new_num = message.payload.decode()
+    print("Received Number: " + new_num)
+
+    num = int(new_num)
+    num += 1
+    time.sleep(1)
+    client.publish("cynthliu/ping", str(num))
+    print(f"Publishing number to ping: {num}")
+
 
 if __name__ == '__main__':
     client = mqtt.Client()
     client.on_connect = on_connect
     client.connect(host="172.20.10.4", port=1883, keepalive=60) #host?
 
-    client.loop_start()
-    time.sleep(1)
-
-    while True:
-        client.publish("cynthliu/ping", f"{num}")
-        print(f"published ping\n")
-        time.sleep(1)
+    client.loop_forever()
